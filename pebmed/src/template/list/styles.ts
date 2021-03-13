053f544a7d43f9ca9213d13a2d9a96bfc12be147
Roberto Marcal <roberto.marcal@stoneage.com.br>
2021-03-13 09:46:27 -0300

    CRUD list template

---
+++ pebmed/src/template/list/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const Container = styled.div`
+	padding: 16px;
+	width: 100vw;
+	height: 100vh;
+`;
+
+export const Title = styled.h1`
+	margin: 0;
+`;
