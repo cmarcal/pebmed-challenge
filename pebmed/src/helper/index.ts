eb8de71997eaefc92ba426df71d7c2fb5873df2b
= <roberto.marcal@stoneage.com.br>
2021-03-21 19:15:17 -0300

    Creating same regex

---
+++ pebmed/src/helper/index.ts
@@ -0,0 +1,13 @@
+export const cpfMaskInput = (value: string) =>
+	value
+		.replace(/\D/g, '')
+		.replace(/(\d{3})(\d)/, '$1.$2')
+		.replace(/(\d{3})(\d)/, '$1.$2')
+		.replace(/(\d{3})(\d{1,2})/, '$1-$2')
+		.replace(/(-\d{2})\d+?$/, '$1');
+
+export const validityCreditCardMask = (value: string) =>
+	value
+		.replace(/\D/g, '')
+		.replace(/(\d{2})(\d)/, '$1/$2')
+		.replace(/(\d{2})\d+?$/, '$1');
